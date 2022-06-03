e96380d597626c3e220ea9739170d34727f6710c
Богданыч <xachwork2@gmail.com>
2022-06-03 11:43:23 +0300

    Inventory-prototype

---
+++ Assets/Codebase/MonoBehaviors/HeroLogic/HeroInventory.cs
@@ -0,0 +1,22 @@
+﻿using Codebase.InventoryLogic;
+using UnityEngine;
+
+namespace Codebase.HeroLogic
+{
+    public class HeroInventory : MonoBehaviour
+    {
+        [SerializeField] private string _openInventoryButton;
+
+        [Space]
+
+        [SerializeField] private InventoryBehavior _inventory;
+
+        private void Update()
+        {
+            if(Input.GetButtonDown(_openInventoryButton))
+            {
+                _inventory.Open();
+            }
+        }
+    }
+}
