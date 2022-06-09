497093233c1161d448ecd697397c477350780e8b
Kirill Panchenko <kreashlir@gmail.com>
2022-06-09 11:50:41 +0300

    Hiding behavior UniRX

--- Assets/Codebase/MonoBehaviors/HidingLogic/HidingObjectBehavior.cs
+++ Assets/Codebase/MonoBehaviors/HidingLogic/HidingObjectBehavior.cs
@@ -1,14 +1,19 @@
+using System;
 using UltEvents;
+using UniRx;
 using UnityEngine;
 
 namespace Codebase.HidingObjectLogic
 {
     public class HidingObjectBehavior : MonoBehaviour
     {
-        [SerializeField] private UltEvent _insideEvent;
-        [SerializeField] private UltEvent _outsideEvent;
-
         [SerializeField] private bool _inside;
+
+        [SerializeField] private Transform _insidePos;
+        [SerializeField] private Transform _outsidePos;
+        [SerializeField] private GameObject _hero;
+
+        private CompositeDisposable _disposible;
 
         public void Interact()
         {
@@ -25,16 +30,45 @@ namespace Codebase.HidingObjectLogic
 
             _inside = !_inside;
         }
-
-        
-
         private void GetInside()
         {
-            _insideEvent.Invoke();
+            float deltaTime = 0;
+            Vector3 startPos = _hero.transform.position;
+
+            Observable.EveryUpdate().Subscribe(action =>
+            {
+                if (deltaTime >= 1)
+                {
+                    _hero.transform.position = _insidePos.position;
+                    _disposible.Clear();
+                }
+                _hero.transform.position = Vector3.Lerp(startPos, _insidePos.position, deltaTime);
+
+                deltaTime += Time.deltaTime;
+
+            }).AddTo(_disposible);
+
+            //_hero.GetComponent<CharacterController>().enabled = true;
         }
         private void GetOutside()
         {
-            _outsideEvent.Invoke();
+            float deltaTime = 0;
+            Vector3 startPos = _insidePos.position;
+
+            Observable.EveryUpdate().Subscribe(action =>
+            {
+                if (deltaTime >= 1)
+                {
+                    _insidePos.position = _outsidePos.position;
+                    _disposible.Clear();
+                }
+                _insidePos.position = Vector3.Lerp(startPos, _outsidePos.position, deltaTime);
+
+                deltaTime += Time.deltaTime;
+
+            }).AddTo(_disposible);
+
+            //_hero.GetComponent<CharacterController>().enabled = false;
         }
     }
 }
