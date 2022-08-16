1d75155ff120559ad84bda690d4ed0a67ece0e11
AliceMon <xachwork2@gmail.com>
2022-08-16 11:03:11 +0300

    fix-door

---
+++ Assets/Codebase/MonoBehaviors/Door/FixDoor.cs
@@ -0,0 +1,18 @@
+﻿using Lean.Transition.Method;
+using UnityEngine;
+
+namespace Codebase.DoorLogic
+{
+    public class FixDoor : MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+        [SerializeField] private LeanTransformLocalRotation _openRotationAnimation;
+        [SerializeField] private LeanTransformLocalRotation _exitRotationAnimation;
+
+        private void OnValidate()
+        {
+            _openRotationAnimation.Data.Rotation = Quaternion.Euler(_transform.eulerAngles + new Vector3(0, 90, 0));
+            _exitRotationAnimation.Data.Rotation = _transform.rotation;
+        }
+    }
+}
